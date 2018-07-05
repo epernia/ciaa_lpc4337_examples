3ddfa6c5aaa584f9ff9fb4847df180ddc645350f
Eric Pernia <ericpernia@gmail.com>
2018-07-05 18:26:24 -0300

    repair project.mk

--- project.mk
+++ project.mk
@@ -4,6 +4,6 @@
 
 # ---------- Project path ----------------------------------------------
 # Project path (relative to ciaa_project folder)
-#PROJECT_PATH = ../ciaa_lpc4337_examples/c/sapi/bare_metal
+PROJECT_PATH = ../ciaa_lpc4337_examples/c/sapi/bare_metal
 # ---------- Project name ----------------------------------------------
-#PROJECT_NAME = sapi_basic_example
+PROJECT_NAME = sapi_basic_example
