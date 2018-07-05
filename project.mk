50c7d1d3fb2204fc1e24145316f775b252496f13
Eric Pernia <ericpernia@gmail.com>
2018-07-05 18:06:35 -0300

    move examples from repo cese-edu-ciaa

---
+++ project.mk
@@ -0,0 +1,9 @@
+#==============================================================================
+# Select a project to compile, or comment and uncomment an example
+#==============================================================================
+
+# ---------- Project path ----------------------------------------------
+# Project path (relative to ciaa_project folder)
+#PROJECT_PATH = ../ciaa_lpc4337_examples/c/sapi/bare_metal
+# ---------- Project name ----------------------------------------------
+#PROJECT_NAME = sapi_basic_example
